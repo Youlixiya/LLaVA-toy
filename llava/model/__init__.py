a9b43819abae4aaaee991f63971e2c19e6aa47c0
yl <2317676993@qq.com>
2024-02-04 13:20:55 +0800

    llava-toy

--- llava/model/__init__.py
+++ llava/model/__init__.py
@@ -1,6 +1,7 @@
 try:
     from .language_model.llava_llama import LlavaLlamaForCausalLM, LlavaConfig
     from .language_model.llava_sam_llama import LlavaSAMLlamaForCausalLM, LlavaSAMLlamaConfig
+    from .language_model.llava_tinyclip_llama import LlavaTinyCLIPLlamaForCausalLM, LlavaTinyCLIPLlamaConfig
     from .language_model.llava_mpt import LlavaMptForCausalLM, LlavaMptConfig
     from .language_model.llava_mistral import LlavaMistralForCausalLM, LlavaMistralConfig
     from .language_model.llava_minicpm import LlavaMiniCPMForCausalLM, LlavaMiniCPMConfig
